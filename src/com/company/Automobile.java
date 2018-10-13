eab775f5431a41a4101a651abb9b45e9008a4f81
woseibonsu <40476387+woseibonsu@users.noreply.github.com>
2018-10-12 21:36:58 -0400

    Finished

--- src/com/company/Automobile.java
+++ src/com/company/Automobile.java
@@ -4,4 +4,27 @@ package com.company;
  * Created by dpennebacker on 10/9/16.
  */
 public class Automobile {
+    double gasMilage;
+    double gasTank;
+
+    public Automobile(double mpg){
+        gasMilage = mpg;
+        gasTank = 0;
+    }
+
+    public void fillUp(double f)
+    {
+        gasTank += f;
+    }
+
+    public void takeTrip(double m)
+    {
+        gasTank -= (m/gasMilage);
+    }
+
+    public double reportFuel()
+    {
+        return gasTank;
+    }
 }
+
